674fa8e25b596f350cd328d27b34c659d4efd87b
Sneha Kapoor <sneha.ofcl1001@gmail.com>
2024-11-15 02:44:09 +0530

    "fixed dropdown alignment"

--- app/src/main/java/com/example/contactformapp/screens/GenderScreen.kt
+++ app/src/main/java/com/example/contactformapp/screens/GenderScreen.kt
@@ -4,16 +4,15 @@ import android.annotation.SuppressLint
 import android.widget.Toast
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material3.Button
@@ -22,7 +21,6 @@ import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -30,16 +28,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.toSize
 import androidx.navigation.NavController
-import androidx.navigation.compose.rememberNavController
 import com.example.contactformapp.PermissionRequest
 import com.example.contactformapp.audiorecorder.playback.AndroidAudioPlayer
 import com.example.contactformapp.audiorecorder.recorder.AndroidAudioRecorder
@@ -82,77 +75,86 @@ fun GenderScreen(modifier: Modifier,
     }
 
     //adjusting icon as per the drop down menu
-    val icon = if(expanded){
-        Icons.Filled.KeyboardArrowUp
-    }
-    else{
-        Icons.Filled.KeyboardArrowDown
-    }
+    val icon = if (expanded) Icons.Filled.KeyboardArrowUp else Icons.Filled.KeyboardArrowDown
 
-    Column(modifier = modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally){
-
-        PermissionRequest(
-            onPermissionGranted = {
-                File(cacheDir, "audio.mp3").also {
-                    recorder.start(it)
-                    audioFile =it
-                }
-            },
-            onPermissionDenied = {
-                Toast.makeText(context, "Microphone permission is required to record audio.", Toast.LENGTH_SHORT).show()
-            }
-        )
-
-        Spacer(modifier = Modifier.height(6.dp))
-
-        Text(text = "Q1. Select your gender",
-            modifier = modifier.padding(6.dp)
-        )
-
-        OutlinedTextField(value = selectedOption,
-            onValueChange = {
-                //selectedOption = it
-            },
-            modifier = modifier
-                //to assign same width to the drop down menu as the text field
-                .onGloballyPositioned { coordinates ->
-                size = coordinates.size //captures size of the text field
-            },
-            label = { Text(text = "Gender")},
-            trailingIcon = {
-                Icon(
-                    icon, contentDescription = "Dropdown Arrow",
-                    modifier = Modifier.clickable { expanded = !expanded })
-            }
-        )
-
-        DropdownMenu(expanded = expanded,
-            onDismissRequest = { expanded = false},
+    Box(modifier = modifier.fillMaxSize()) {
+        Column(
             modifier = Modifier
-                .width(with(LocalDensity.current) { size.width.toDp() })
-                .offset(y = with(LocalDensity.current) { size.height.toDp() }) //position dropdown below the text field
+                .align(Alignment.Center)
+                .fillMaxWidth(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            options.forEach{lable->
-                DropdownMenuItem(
-                    text = { "Gender" },
-                    onClick = {
-                        selectedOption = lable
-                        expanded = false
+            PermissionRequest(
+                onPermissionGranted = {
+                    File(cacheDir, "audio.mp3").also {
+                        recorder.start(it)
+                        audioFile = it
+                    }
+                },
+                onPermissionDenied = {
+                    Toast.makeText(
+                        context,
+                        "Microphone permission is required to record audio.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            )
+
+            Spacer(modifier = Modifier.height(6.dp))
+
+            Text(
+                text = "Q1. Select your gender",
+                modifier = modifier.padding(6.dp)
+            )
+
+            Box(
+                modifier = modifier
+                    .wrapContentSize(Alignment.TopEnd)
+            ) {
+                OutlinedTextField(value = selectedOption,
+                    onValueChange = {
+                        //selectedOption = it
+                    },
+                    modifier = modifier
+                        //to assign same width to the drop down menu as the text field
+                        .onGloballyPositioned { coordinates ->
+                            size = coordinates.size //captures size of the text field
+                        },
+                    label = { Text(text = "Gender") },
+                    trailingIcon = {
+                        Icon(
+                            icon, contentDescription = "Dropdown Arrow",
+                            modifier = Modifier.clickable { expanded = !expanded })
                     }
                 )
+
+                DropdownMenu(
+                    expanded = expanded,
+                    onDismissRequest = { expanded = false },
+                    modifier = Modifier
+                        .align(Alignment.Center)
+//                .width(with(LocalDensity.current) { size.width.toDp() })
+//                .offset(y = with(LocalDensity.current) { size.height.toDp() }) //position dropdown below the text field
+                ) {
+                    options.forEach { label ->
+                        DropdownMenuItem(
+                            text = { Text(text = label) },
+                            onClick = {
+                                selectedOption = label
+                                expanded = false
+                            }
+                        )
+                    }
+                }
+            }
+
+            Button(onClick = {
+                navController.navigate("AgeScreen")
+            }) {
+                Text(text = "Next")
             }
         }
-
-        Button(onClick = {
-            navController.navigate("AgeScreen")
-        }) {
-            Text(text = "Next")
-        }
-
-
-
     }
 }
 
